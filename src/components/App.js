f4f1e045c01811c4212163952dae9345370a32d7
Anna J <ajeszeck@gmail.com>
2018-06-22 22:55:17 -0700

    Add account, home, sign in, and sign up page views - all empty currently, routing to them works.

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import '../App.css';
 import base from '../base.js';
 import Plant from './Plant.js';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from './Navigation.js';
+import SignInPage from './SignInPage.js';
+import SignUpPage from './SignUpPage.js';
+import Account from './Account.js';
+import Home from './Home.js';
+
+import * as routes from '../constants/routes.js';
 
 class App extends Component {
   constructor(props) {
@@ -39,7 +45,14 @@ class App extends Component {
           <h1 className="App-title">Welcome to Water Me</h1>
         </header>
         <Router>
-          <Navigation />
+          <div>
+            <Navigation />
+            <hr/>
+            <Route path={routes.SIGN_IN} component={() => <SignInPage />} />
+            <Route path={routes.SIGN_UP} component={() => <SignUpPage />} />
+            <Route path={routes.ACCOUNT} component={() => <Account />} />
+            <Route path={routes.HOME} component={() => <Home />} />
+          </div>
         </Router>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/components/SignInPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const SignInPage = () =>
+  <div>
+    <h1>Sign In Page</h1>
+    <form>
+      <input type="text"/>
+    </form>
+  </div>
+
+export default SignInPage;
